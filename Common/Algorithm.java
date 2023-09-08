b8f583273f24ac15b966a330f2ddca0024e1ac9c
nhathuy <nguyenkhoanhathuy@gmail.com>
2023-09-08 09:22:19 +0700

    sort and search

--- Common/Algorithm.java
+++ Common/Algorithm.java
@@ -49,4 +49,33 @@ public class Algorithm {
         }
     }
 
+    public int binarySearch(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+
+    public int linerSearch(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
 }
--- Controller/SortProgramming.java
+++ Controller/SortProgramming.java
@@ -8,7 +8,7 @@ import View.Menu;
 
 public class SortProgramming extends Menu<String> {
 
-    static String[] mcSort = { "Buddle Sort", "Quick Sort", "Exit" };
+    static String[] option = { "search", "sort", "Exit" };
 
     protected Library library;
     protected Algorithm algorithm;
@@ -16,7 +16,7 @@ public class SortProgramming extends Menu<String> {
     protected int size_array;
 
     public SortProgramming(Element element) {
-        super("Sort Option", mcSort);
+        super("All option", option);
         library = new Library();
         algorithm = new Algorithm();
         size_array = element.getSize_array();
@@ -27,31 +27,73 @@ public class SortProgramming extends Menu<String> {
 
     @Override
     public void execute(int n) {
-
-        System.out.println("Unsorted array: ");
-        library.display(array);
         switch (n) {
             case 1:
-                algorithm.buddleSort(array);
-                System.out.println("\nSorted array by Buddle Sort: ");
-                library.display(array);
-                System.out.println("");
-                System.exit(0);
+                String mcSearch[] = new String[] { "Linear Search", "Binary Search", "Exit" };
 
+                Menu sortMenu = new Menu("Searching", mcSearch) {
+                    @Override
+                    public void execute(int choice) {
+                        algorithm.buddleSort(array);
+                        System.out.println("array: ");
+                        library.display(array);
+                        System.out.println(" Searching by" + mcSearch[n]);
+                        int numberSearch;
+                        switch (choice) {
+                            case 1:
+                                numberSearch = library.getInt("Enter the number to find ", 1, 100);
+                                System.out.println(algorithm.linerSearch(array, numberSearch));
+
+                                break;
+                            case 2:
+                                numberSearch = library.getInt("Enter the number to find ", 1, 100);
+                                System.out.println(algorithm.binarySearch(array, numberSearch));
+                                break;
+                            case 3:
+                                System.exit(0);
+                                break;
+                        }
+                    }
+
+                };
+                sortMenu.runSearch();
                 break;
             case 2:
-                algorithm.quickSort(array, 0, size_array - 1);
-                System.out.println("\nSorted array by Quick Sort: ");
-                library.display(array);
-                System.out.println("");
-                System.exit(0);
+                String[] mcSort = { "Buddle Sort", "Quick Sort", "Exit" };
 
+                Menu searchMenu = new Menu("sort", mcSort) {
+                    @Override
+                    public void execute(int choice) {
+                        System.out.println("Unsorted array: ");
+                        library.display(array);
+                        switch (choice) {
+                            case 1:
+                                algorithm.buddleSort(array);
+                                System.out.println("\nSorted array by Buddle Sort: ");
+                                library.display(array);
+                                System.out.println("");
+                                System.exit(0);
+                                break;
+                            case 2:
+                                algorithm.quickSort(array, 0, size_array - 1);
+                                System.out.println("\nSorted array by Quick Sort: ");
+                                library.display(array);
+                                System.out.println("");
+                                System.exit(0);
+                                break;
+                            case 3:
+                                System.exit(0);
+                                break;
+                        }
+                        SortProgramming SP = new SortProgramming(element);
+                        SP.run();
+                    }
+                };
+                searchMenu.run();
                 break;
             case 3:
-                System.exit(0);
                 break;
+
         }
-        SortProgramming SP = new SortProgramming(element);
-        SP.run();
     }
 }
